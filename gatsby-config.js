88b66b3fefc786dbcae98633666b76a273b2afd1
Scott Williams <tocsoft@gmail.com>
2020-01-11 17:33:03 +0000

    andd google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-156060337-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
